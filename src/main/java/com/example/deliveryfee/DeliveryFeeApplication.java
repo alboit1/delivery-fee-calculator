bddd0e1d2961049a85ca154a3eaff691c93a1f49
alboit <boitsukalika@gmail.com>
2026-03-26 22:17:24 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/deliveryfee/DeliveryFeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryFeeApplication.class, args);
+    }
+
+}
